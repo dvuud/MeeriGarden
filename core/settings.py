2529d0ff84c5a68766d097499240bd89e742b4dd
dvuud <baltabaevdavud88@gmail.com>
2024-12-28 23:13:56 +0600

    new commit

--- core/settings.py
+++ core/settings.py
@@ -130,4 +130,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 # вв
+# врпарпра
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
