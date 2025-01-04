cfb610d4c7711daa685bb2ec8e1fb123405823b8
dvuud <baltabaevdavud88@gmail.com>
2025-01-04 23:08:17 +0600

    6th commit

--- core/settings.py
+++ core/settings.py
@@ -130,7 +130,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 # вв
-# врпарпра
+врпарпра
 # jfdjfjd
 # dhsdhjghsd
 # djsdjsdj
